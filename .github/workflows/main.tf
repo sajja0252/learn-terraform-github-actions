ae6bda223184565f892c776f6c85d1761f473138
sajja0252 <55823052+sajja0252@users.noreply.github.com>
2022-11-29 14:45:51 +0530

    token updated

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -18,5 +18,6 @@ terraform {
    workspaces {
       name = "gh-actions-demo"
     }
+    token = "ztgnfU9zgsQ1sw.atlasv1.ywg10fy27B0NF199CyzaU7IYLYEv7UJRpxEyay9kLGx4eOa7SjkGNYlAdKdUbcyxZIU"
   }
 }
